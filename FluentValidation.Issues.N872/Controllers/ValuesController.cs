36d5a93a731e92a0c12cfd9597bb1618ff06211a
Deilan <deilan@users.noreply.github.com>
2018-08-20 14:49:46 +0300

    prepare a sample for https://github.com/JeremySkinner/FluentValidation/issues/872

---
+++ FluentValidation.Issues.N872/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentValidation.Issues.N872.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(GetQuery query)
+        {
+            return new[] { "value1", "value2" };
+        }
+    }
+
+    public class GetQuery
+    {
+    }
+}
--- FluentValidation.Issues.N872/Startup.cs
+++ FluentValidation.Issues.N872/Startup.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace FluentValidation.Issues
+namespace FluentValidation.Issues.N872
 {
     public class Startup
     {
@@ -24,7 +19,9 @@ namespace FluentValidation.Issues
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .AddFluentValidation()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
